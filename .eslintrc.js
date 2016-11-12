e1caa8d8187fe22a088c612758a27008a4ab4066
Sammi A <sammialldredge@gmail.com>
2016-11-12 13:47:45 -0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,12 @@ module.exports = {
         "jsx-a11y",
         "import"
     ],
+    "parser": "babel-eslint",
     "rules": {
         "no-console":0,
         "no-underscore-dangle":0,
         "react/jsx-filename-extension":0,
         "max-len":0,
+        "import/newline-after-import":0,
     }
 };
