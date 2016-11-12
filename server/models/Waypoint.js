5be9b165cad1dcda35c680f3a672d683ac773f6d
Sammi A <sammialldredge@gmail.com>
2016-11-11 19:20:31 -0800

    merging

--- server/models/Waypoint.js
+++ server/models/Waypoint.js
@@ -4,11 +4,7 @@ const Schema = mongoose.Schema
 const waypointSchema = new Schema({
   waypointName: { type: String, required: true },
   pointsOfInterest: [{ type: Schema.Types.ObjectId, ref: 'PointOfInterest' }],
-<<<<<<< HEAD
-  falseRoute: {type: Array},
-=======
   falseRoute: {type: Array}
->>>>>>> 79ad8599d30474f3a54fb6360569d0257181da42
 })
 
 const Waypoint = mongoose.model('Waypoint', waypointSchema)
