488c4bb80c7149608ddfc4cec542d2eb0f568733
Joshua Maddox <joshua@cinderhousecreative.com>
2016-11-12 20:39:06 -0800

    added in coords in both Waypoint and InitialState

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,5 +1,59 @@
+let waypoint = {
+    waypointName: 'Danielville, Samtucky',
+    coords: { lat: 40.712784, lng: -74.005941 },
+    links: ['https://i.ytimg.com/vi/c7oV1T2j5mc/maxresdefault.jpg', 'http://feelgrafix.com/data/landscape/landscape-1.jpg'],
+    text: 'Bacon ipsum dolor amet pork loin spare ribs corned beef pork chop drumstick ham hock pancetta ribeye filet mignon. Rump shank tenderloin meatloaf meatball t-bone. Tail boudin porchetta kielbasa. Bresaola corned beef strip steak, bacon sirloin pork belly short loin shankle chuck pork ground round fatback ball tip pork loin tri-tip',
+    pointsOfInterest: [
+      {
+        poiName: 'El Danieleo Dictador',
+        clues: [
+          {
+            waypoint: "DanielTown, Richardington",
+            poiName: "Sir Daniel's Iron Hammer",
+            links: ['http://feelgrafix.com/data/landscape/landscape-17.jpg', 'http://feelgrafix.com/data/landscape/landscape-3.jpg' ],
+            text: 'Here marks the spot where the communist Daniel struck a developer for misplacing a semi-colon. With the infamous words "Work harder, Work Better or feel the hammer", Daniel built his first public facing App starting a long nightmarish reign'
+          },
+          {
+            waypoint: "DanielTown, Richardington",
+            poiName: "Sir Daniel's Iron Hammer",
+            links: ['http://feelgrafix.com/data/landscape/landscape-17.jpg', 'http://feelgrafix.com/data/landscape/landscape-3.jpg' ],
+            text: 'Here marks the spot where the communist Daniel struck a developer for misplacing a semi-colon. With the infamous words "Work harder, Work Better or feel the hammer", Daniel built his first public facing App starting a long nightmarish reign'
+          }
+        ],
+        links: ['http://feelgrafix.com/data/landscape/landscape-17.jpg', 'http://feelgrafix.com/data/landscape/landscape-3.jpg' ],
+        text: 'Also known as "Daniel the Dictator", the El Danieleo Dictador was the presidential palace where daniel tortured developers and, through shouts and hard labor, killed over 4,000 coders'
+      }
+    ],
+    falseRoute: [
+      { 
+        falseRouteName: 'So Long Sammi Hearth Stone',
+        coords: {lat: 38.975531, lng: -85.579298 },
+        links: ['http://ixlmasonry.com/wp-content/uploads/2016/02/Centre-Hearth-Stone-shown-in-Summer-Wheat-Travertine-finish.jpg', 'http://feelgrafix.com/data/landscape/landscape-1.jpg'],
+        text: "The So Long Sammi Hearth Stone was set by the Late Richard of CodingDale after Sammi was struck down by Daniels Iron Hammer. Sammi was reported struck for pulling when she should've fetched"
+      }, 
+      { 
+        falseRouteName: 'Coders Grave of Danielopolis',
+        coords: {lat: 39.904211, lng: 116.407395 },
+        links: ['https://i.ytimg.com/vi/c7oV1T2j5mc/maxresdefault.jpg', 'http://feelgrafix.com/data/landscape/landscape-1.jpg'],
+        text: 'The mass grave sites where Daniel the Designing Dictator had the victims of his iron ruled tossed away.'
+      }
+    ]
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default {
-  scenarios: [],
+  scenarios: [waypoint, waypoint, ],
   scenario: {},
   authenticated: false,
   user: {},
