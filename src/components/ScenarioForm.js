c49b8b43d4950b3856a778d405bfd2ad6e697a37
Sammi A <sammialldredge@gmail.com>
2016-11-15 00:24:48 -0800

    form update

--- src/components/ScenarioForm.js
+++ src/components/ScenarioForm.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Geosuggest from 'react-geosuggest'
 import PoiForm from './PoiForm'
+import TextInput from './CreateScenario/TextInput'
 
 export default class ScenarioForm extends Component {
   constructor() {
     super()
     this.state = {
-      fieldCount: 1
+      fieldCount: 1,
+      falseRouteCount: 1
     }
   }
 
@@ -24,10 +26,25 @@ export default class ScenarioForm extends Component {
     }
   }
 
+  addFalseRoute() {
+    let { falseRouteCount } = this.state
+    falseRouteCount += 1
+    this.setState({ falseRouteCount })
+  }
+
+  removeFalseRoute() {
+    let { falseRouteCount } = this.state
+    if (falseRouteCount > 0) {
+      falseRouteCount -= 1
+      this.setState({ falseRouteCount })
+    }
+  }
+
   render() {
     const { onChange, newScenario, onClick, onSelect, getLabel } = this.props
-    const { fieldCount, poiCount } = this.state
+    const { fieldCount, falseRouteCount } = this.state
     let waypointFields = []
+    let falseRouteFields = []
     for (let i = 1; i <= fieldCount; i += 1) {
       const theName = `waypoint${i}`
       const newField = (
@@ -41,25 +58,43 @@ export default class ScenarioForm extends Component {
           onSuggestSelect={onSelect}
           getSuggestLabel={getLabel}
           placeholder="Search Waypoint"
-        />
+                                    />
           <PoiForm key={`PoiForm${i}`} onChange={onChange} newScenario={newScenario} onClick={onClick} onSelect={onSelect} name={theName} />
         </div>)
       waypointFields = [...waypointFields, newField]
+    }
+    for (let j = 1; j <= falseRouteCount; j += 1) {
+      const routeName = `falseRoute${j}`
+      const newFalseRoute = (
+        <Geosuggest
+          key={routeName}
+          id={routeName}
+          type="text"
+          onKeyPress={onChange}
+          name={routeName}
+          value={newScenario[routeName]}
+          // label={theName}
+          onSuggestSelect={onSelect}
+          placeholder="Add False Route"
+        />)
+      falseRouteFields = [...falseRouteFields, newFalseRoute]
     }
 
 
     return (
       <form className="creationForm">
-        <Geosuggest
-          id='startLocation'
-          type="text"
-          onKeyPress={onChange}
-          name="startLocation"
-          value={newScenario.startLocation}
-          // label="Start Location"
-          onSuggestSelect={onSelect}
-          getSuggestLabel={getLabel}
-          placeholder="Search Start Location"
+        <TextInput id="scenarioName" name="scenarioName" value={newScenario.scenarioName} onChange={onChange} type="text" label="Scenario Name" />
+
+          <Geosuggest
+            id='startLocation'
+            type="text"
+            onKeyPress={onChange}
+            name="startLocation"
+            value={newScenario.startLocation}
+            // label="Start Location"
+            onSuggestSelect={onSelect}
+            getSuggestLabel={getLabel}
+            placeholder="Search Start Location"
         />
         <PoiForm onChange={onChange} newScenario={newScenario} onClick={onClick} onSelect={onSelect} name='startLocation'/>
         {waypointFields}
@@ -77,6 +112,9 @@ export default class ScenarioForm extends Component {
         <PoiForm onChange={onChange} newScenario={newScenario} onClick={onClick} onSelect={onSelect} name='endLocation'/>
         <button type="button" onClick={() => this.addWaypointInput()}>Add WayPoint</button>
         <button type="button" onClick={() => this.removeWaypointInput()}>Delete WayPoint</button>
+        {falseRouteFields}
+        <button type="button" onClick={() => this.addFalseRoute()}>Add False Route</button>
+        <button type="button" onClick={() => this.removeFalseRoute()}>Delete False Route</button>
         <button className="btnClass" type="submit" onClick={onClick}>Submit Scenario</button>
       </form>
     )
