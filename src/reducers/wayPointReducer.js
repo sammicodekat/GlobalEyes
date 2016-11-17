2e1920979034da50601c217f6948054f8942f715
Sammi A <sammialldredge@gmail.com>
2016-11-17 01:37:45 -0800

    initial state

--- src/reducers/wayPointReducer.js
+++ src/reducers/wayPointReducer.js
@@ -1,7 +1,7 @@
 import * as types from '../actions/actionTypes'
 import initialState from './initialState'
 
-export default function wayPointReducer(state = initialState.waypoint, action) {
+export default function wayPointReducer(state = {}, action) {
   switch (action.type) {
     case types.RECEIVE_WAYPOINT_SUCCESS:
       return action.waypoint
