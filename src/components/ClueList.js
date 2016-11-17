92434faa4e42dc749ceafa7bd10fc7a5dd3dc1f8
Sammi A <sammialldredge@gmail.com>
2016-11-17 00:46:17 -0800

    meow

--- src/components/ClueList.js
+++ src/components/ClueList.js
@@ -3,8 +3,8 @@ import React from 'react'
 const renderClueList = clues => (
   <div className="clueList">
     <h3>A clue perhaps?</h3>
-    <img src={clues[0].links[0]} />
-    <p>{clues[0].text}</p>
+    <img src={clues.links} />
+    <p>{clues.text}</p>
   </div>
 )
 
