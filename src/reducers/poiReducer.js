abedfee63588502593a8708172d929c6f8b851e6
Sammi A <sammialldredge@gmail.com>
2016-11-17 01:08:59 -0800

    s

--- src/reducers/poiReducer.js
+++ src/reducers/poiReducer.js
@@ -2,8 +2,8 @@ import * as types from '../actions/actionTypes'
 
 export default function poiReducer(state = {}, action) {
   switch (action.type) {
-   console.log('action.poi',action.poi)
     case types.RECEIVE_POI_SUCCESS:
+    console.log('action.poi',action.poi)
       return action.poi
     default:
       return state
