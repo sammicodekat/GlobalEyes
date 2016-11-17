dfad3ca1cbee44b44d174d93c3c7bbff9c4a1578
mavarius <metalwhirlwind@yahoo.com>
2016-11-17 10:10:33 -0800

    continue form format

--- src/components/CreateScenario/ScenarioForm.js
+++ src/components/CreateScenario/ScenarioForm.js
@@ -7,7 +7,7 @@ export default class ScenarioForm extends Component {
   constructor() {
     super()
     this.state = {
-      waypointCount: 0
+      waypointCount: 1
     }
   }
 
@@ -19,7 +19,7 @@ export default class ScenarioForm extends Component {
 
   removeWaypointInput() {
     let { waypointCount } = this.state
-    if (waypointCount > 0) {
+    if (waypointCount > 1) {
       waypointCount -= 1
       this.setState({ waypointCount })
     }
@@ -29,11 +29,19 @@ export default class ScenarioForm extends Component {
     const { onChange, newScenario, onClick } = this.props
     const { waypointCount } = this.state
     // const deleteButton = waypointCount ? (<button type="button" onClick={() => this.removeWaypointInput()}>Delete WayPoint</button>) : ''
+    let waypointForms = []
+
+    for (let i = 0; i < waypointCount; i += 1) {
+      const newForm = <WayPointForm key={`WayPointForm${i}`} {...this.props} waypointCount={waypointCount} />
+      waypointForms = [...waypointForms, newForm]
+    }
+
     return (
       <form className="creationForm">
         <TextInput id="scenarioName" name="scenarioName" value={newScenario.scenarioName} onChange={onChange} type="text" placeholder="Enter Scenario Name" />
         {/* {deleteButton} */}
-        <WayPointForm {...this.props} waypointCount={waypointCount} />
+        {waypointForms}
+        <button className="addWaypointBtn" type="button" onClick={() => this.addWaypointInput()}><span>+</span>Add WayPoint</button>
         <button className="btnClass" type="submit" onClick={onClick}>Submit Scenario</button>
       </form>
     )
