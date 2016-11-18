15b3a3de552151307e868f57eefa00f463ac462d
Sammi A <sammialldredge@gmail.com>
2016-11-18 14:08:42 -0800

    m

--- src/components/PlaceList.js
+++ src/components/PlaceList.js
@@ -1,22 +1,30 @@
 import React from 'react'
-import { browserHistory } from 'react-router'
+import {browserHistory} from 'react-router'
 
-const renderPlaceList = (scenarioId, visited, nextplaces, updateUsersWaypoint ) => {
-console.log('visited',visited)
-console.log('nextplaces',nextplaces)
-  visited.map((waypoint, i) => (
-  <button className='visited' key={i} onClick={() => {browserHistory.replace(`/${scenarioId}/location/${waypoint._id}`), updateUsersWaypoint(waypoint._id, waypoint.coords) }}>{waypoint.waypointName}</button>
-))
-  nextplaces.map((waypoint, i) => (
-  <button className='nextPlaces' key={i+20} onClick={() => {browserHistory.replace(`/${scenarioId}/location/${waypoint._id}`), updateUsersWaypoint(waypoint._id, waypoint.coords) } }>{waypoint.waypointName}</button>
-))
- }
+const renderVisited = (scenarioId, visited, nextplaces, updateUsersWaypoint) => (
+    visited.map((waypoint, i) => (
+        <button className='visited' key={i} onClick={() => {
+            browserHistory.replace(`/${scenarioId}/location/${waypoint._id}`),
+            updateUsersWaypoint(waypoint._id, waypoint.coords)
+        }}>{waypoint.waypointName}</button>
+    ))
+)
+
+const renderNext = (scenarioId, visited, nextplaces, updateUsersWaypoint) => (
+    nextplaces.map((waypoint, i) => (
+        <button className='nextPlaces' key={i + 20} onClick={() => {
+            browserHistory.replace(`/${scenarioId}/location/${waypoint._id}`),
+            updateUsersWaypoint(waypoint._id, waypoint.coords)
+        }}>{waypoint.waypointName}</button>
+    ))
+)
 
 const PlaceList = (props) => (
-  <div>
-    <h4>hihih</h4>
-    {renderPlaceList(props.scenarioId, props.visited, props.nextplaces, props.updateUsersWaypoint)}
-  </div>
+    <div>
+      <h4>hihih</h4>
+      {renderVisited(props.scenarioId, props.visited, props.nextplaces, props.updateUsersWaypoint)}
+      {renderNext(props.scenarioId, props.visited, props.nextplaces, props.updateUsersWaypoint)}
+    </div>
 )
 
 export default PlaceList
