bd2c2518227c3ab715251a0a54f1eecbc1f90233
Joshua Maddox <joshua@cinderhousecreative.com>
2016-11-18 23:55:00 -0800

    pushing an erro by command of Daniel

---
+++ src/components/GameOverPage.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+// import { browserHistory } from 'react-router'
+import { updateUserObject } from '../actions/auth'
+
+class GameOverPage extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  placesVisted = () => {
+    const { userObj, scenario } = this.props
+    return userObj.visitedWaypoints.map((wp, i)=> {
+      if(wp === scenario.waypoints[i]._id) {
+         return (
+          <li key={i}>{scenario.waypoints[i].waypointName}</li>
+        )
+      }
+    })
+  }
+ 
+  render() {
+    const randomBackground = {
+      backgroundImage: `url(/images/background${Math.floor(Math.random() * 2) + 1}.jpg)`
+    }
+    return (
+      <div className="introPage">
+        <div className="backgroundImage" style={randomBackground} />
+        <div className="pageContent">
+          <div className="intro">
+            <h1>{`Sorry ${this.props.userObj.userName} You've Run Out of Voucher ${this.props.scenario.scenarioName}`}</h1>
+            <div className="greeting">
+              <h3>Your travel stats: </h3>
+              <p>You visited <b>{`${this.props.scenario.vouchers - this.props.userObj.vouchers}`}</b> amazing places:</p>
+              <ul>{this.placesVisted()}</ul> 
+              <p>You used <b>{`${this.props.scenario.vouchers - this.props.userObj.vouchers}`}</b> <i>travel vouchers</i> on your journey</p>
+              <p>You have <b>{`${this.props.userObj.vouchers}`}</b> <i>travel vouchers</i> remaining</p>
+            </div>
+            <div className="introVouchers">
+              <button>Start Another Adventure</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    userObj: state.userObj,
+    scenario: state.scenario
+  }
+}
+
+export default connect(mapStateToProps, null)(GameOverPage)
